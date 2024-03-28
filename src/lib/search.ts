4ddf03c4000895721de1592c1202e0757334ca7f
Adrian <adrhc@outlook.com>
2024-03-28 19:36:51 +0200

    API_PATH

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,11 +1,13 @@
+const API_PATH = '/autorizatii-de-construire-certificate-de-urbanism/api';
+
 export default async function search(query: string): Promise<string[]> {
   //   return ['link1', 'link2', 'link3'];
   const params = new URLSearchParams({ query });
-  console.log(`/api/search?${params}`);
-  return await fetch(`/api/search?${params}`)
+  console.log(`${API_PATH}/search?${params}`);
+  return await fetch(`${API_PATH}/search?${params}`)
     .then((res) => {
-        console.log('res:', res);
-        return res.json();
+      console.log('res:', res);
+      return res.json();
     })
     .catch((e) => [e.toString()]);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: "https://adrhc.go.ro/autorizatii-de-construire-certificate-de-urbanism",
+      '/autorizatii-de-construire-certificate-de-urbanism': {
+        target: "https://adrhc.go.ro",
         changeOrigin: true,
       },
     },
