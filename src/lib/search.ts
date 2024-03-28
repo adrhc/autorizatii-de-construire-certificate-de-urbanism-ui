43828b9e6f3bd09fe1a3f082c84cdbb87277bcea
Adrian <adrhc@outlook.com>
2024-03-28 19:40:40 +0200

    dev

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -3,10 +3,10 @@ const API_PATH = '/autorizatii-de-construire-certificate-de-urbanism/api';
 export default async function search(query: string): Promise<string[]> {
   //   return ['link1', 'link2', 'link3'];
   const params = new URLSearchParams({ query });
-  console.log(`${API_PATH}/search?${params}`);
+//   console.log(`${API_PATH}/search?${params}`);
   return await fetch(`${API_PATH}/search?${params}`)
     .then((res) => {
-      console.log('res:', res);
+    //   console.log('res:', res);
       return res.json();
     })
     .catch((e) => [e.toString()]);
