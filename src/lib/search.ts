c9fe59dfb2be021231479e116a60abfa42a13e67
Adrian <adrhc@outlook.com>
2024-03-28 18:34:48 +0200

    proxy

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,3 +1,11 @@
-export default async function search(query : string): Promise<string[]> {
-  return ['link1', 'link2', 'link3'];
+export default async function search(query: string): Promise<string[]> {
+  //   return ['link1', 'link2', 'link3'];
+  const params = new URLSearchParams({ query });
+  console.log(`/api/search?${params}`);
+  return await fetch(`/api/search?${params}`)
+    .then((res) => {
+        console.log('res:', res);
+        return res.json();
+    })
+    .catch((e) => [e.toString()]);
 }
